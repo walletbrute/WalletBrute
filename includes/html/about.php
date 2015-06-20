36e355c9ed46ca246458677fd339cb4b0e465426
WalleBrute.com <walletbrute@gmail.com>
2015-06-20 05:40:50 -0500

    Added ressource and net usaage graph to about page.

--- includes/html/about.php
+++ includes/html/about.php
@@ -1,3 +1,31 @@
 <blockquote>
 	<p>Are you kidding? We don't even know. Yet.</p>
 </blockquote>
+<div style="display: block;padding: 9.5px;margin: 0 0 10px;font-size: 13px;line-height: 1.42857143;color: #333;word-break: break-all;word-wrap: break-word;background-color: #f5f5f5;border: 1px solid #ccc;border-radius: 4px;text-align:center;margin-left:auto;margin-right:auto;">
+<table cellspacing="5" cellpadding="0" width="1" bgcolor="f5f5f5" border="1" style="width:100%;">
+    <tbody>
+    <tr>
+    <td bgcolor="333333" style="width:70%;">
+      <a href="javascript:void(window.open('http://joestrusz.com:8080/monitorix/imgs/system1z.1day.png','','width=897,height=370,scrollbars=0,resizable=0'))"><img src="http://joestrusz.com:8080/monitorix/imgs/system1.1day.png" border="0" style="height:100%;width:100%;"></a>
+    </td>
+    <td bgcolor="333333" style="width:30%;">
+      <a href="javascript:void(window.open('http://joestrusz.com:8080/monitorix/imgs/system2z.1day.png','','width=895,height=367,scrollbars=0,resizable=0'))"><img src="http://joestrusz.com:8080/monitorix/imgs/system2.1day.png" border="0" style="width:100%;height:50%;"></a>
+      <a href="javascript:void(window.open('http://joestrusz.com:8080/monitorix/imgs/system3z.1day.png','','width=895,height=367,scrollbars=0,resizable=0'))"><img src="http://joestrusz.com:8080/monitorix/imgs/system3.1day.png" border="0" style="width:100%;height:50%;"></a>
+    </td>
+    </tr>
+  </tbody></table>
+  <br />
+  <table cellspacing="5" cellpadding="0" width="1" bgcolor="888888" border="1" style="width:100%;">
+    <tbody>
+    <tr>
+    <td bgcolor="333333" style="width:70%;">
+      <a href="javascript:void(window.open('http://joestrusz.com:8080/monitorix/imgs/net01z.1day.png','','width=897,height=370,scrollbars=0,resizable=0'))"><img src="http://joestrusz.com:8080/monitorix/imgs/net01.1day.png" border="0" style="width:100%;height:100%;"></a>
+    </td>
+    <td valign="top" bgcolor="333333" style="width:30%;">
+      <a href="javascript:void(window.open('http://joestrusz.com:8080/monitorix/imgs/net02z.1day.png','','width=895,height=367,scrollbars=0,resizable=0'))"><img src="http://joestrusz.com:8080/monitorix/imgs/net02.1day.png" border="0" style="width:100%;height:50%;"></a>
+      <a href="javascript:void(window.open('http://joestrusz.com:8080/monitorix/imgs/net03z.1day.png','','width=895,height=367,scrollbars=0,resizable=0'))"><img src="http://joestrusz.com:8080/monitorix/imgs/net03.1day.png" border="0" style="width:100%;height:50%;"></a>
+    </td>
+    </tr>
+  </tbody></table>
+  
+</div>
--- includes/processor/functions.php
+++ includes/processor/functions.php
@@ -4,7 +4,7 @@ function appVersion() {
     system('git log --pretty=format:"%h" -n 1');
 }
 function appDevStatus() {
-	system("git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -- | ".APP_INCLUDES."/bin/ansi2html.sh > ".APP_INCLUDES."/temp/log.html");
+	system("git log -n 40 --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -- | ".APP_INCLUDES."/bin/ansi2html.sh > ".APP_INCLUDES."/temp/log.html");
 }
 function appCurVer() {
 	system('git log -1 --pretty=format:"%h"');
